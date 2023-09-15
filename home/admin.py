c5d03dd5b3cc2c591ff62fd97286e4dcf677fa9f
DJGuruji <nath93266@gmail.com>
2023-09-15 11:39:15 +0530

    admin view for model booking and deparents changed

--- home/admin.py
+++ home/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from .models import *
 
+class DoctorAdmin(admin.ModelAdmin):
+  list_display = ('id','doc_name','doc_spec','dep_name')
+  
+class BookingAdmin(admin.ModelAdmin):
+  list_display = ('id','p_name','p_phone','p_email','doc_name','booked_on')
+  
 admin.site.register(Department)
-admin.site.register(Doctor)
-admin.site.register(Booking)
+admin.site.register(Doctor,DoctorAdmin)
+admin.site.register(Booking, BookingAdmin)
 
