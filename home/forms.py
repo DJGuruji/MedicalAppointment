7bed68782c8c81822008c2b353227da1d549f922
DJGuruji <nath93266@gmail.com>
2023-09-16 09:45:34 +0530

    new time field added to booking model and nav of confirmation modified

--- home/forms.py
+++ home/forms.py
@@ -9,16 +9,21 @@ class CreateUserForm(UserCreationForm):
     model = User
     fields = ['username', 'email', 'password1', 'password2']
   
+
 class DateInput(forms.DateInput):
   input_type = 'date'
-  
+class TimeInput(forms.TimeInput):
+  input_type = 'time'
+    
 class Bookingform(forms.ModelForm):
   class Meta:
     model = Booking
     fields = '__all__'
     
     widgets = {
-      'booking_date' : DateInput(attrs={'min':datetime.date.today()}),
+      'booking_date' : DateInput(attrs={'min': datetime.date.today() }),
+      'booking_time' : TimeInput(attrs={'min': '8', 'max': '17'})
+      
     }
     
     labels = {
@@ -26,7 +31,8 @@ class Bookingform(forms.ModelForm):
       'p_phone' : 'Patient Phone',
       'p_email' : 'Patient Email',
       'doc_name' : 'Choose Doctor',
-      'booked_on' : 'Choose Date',
+      'booking_date' : 'Choose Date',
+      'booking_time':'Choose Time'
       
     }
     
--- home/models.py
+++ home/models.py
@@ -1,28 +1,29 @@
 from django.db import models
 
 class Department(models.Model):
-  dep_name = models.CharField(max_length= 100)
-  dep_desc = models.TextField()
+  dep_name = models.CharField(max_length= 100,null=True)
+  dep_desc = models.TextField(null=True)
   
   def __str__(self):
     return self.dep_name
   
   
 class Doctor(models.Model):
-  doc_name = models.CharField(max_length=100)
-  doc_spec = models.CharField(max_length=100)
-  dep_name = models.ForeignKey(Department,on_delete= models.CASCADE)
+  doc_name = models.CharField(max_length=100,null=True)
+  doc_spec = models.CharField(max_length=100, null=True)
+  dep_name = models.ForeignKey(Department,on_delete= models.CASCADE,null=True)
   
   def __str__(self):
     return 'Dr.'+self.doc_name+'-('+self.doc_spec+')'
 
 
 class Booking(models.Model):
-  p_name = models.CharField(max_length=100)
-  p_phone = models.CharField(max_length=10)
+  p_name = models.CharField(max_length=100,null=True)
+  p_phone = models.CharField(max_length=10,null = True)
   p_email = models.EmailField()
   doc_name = models.ForeignKey(Doctor, on_delete = models.CASCADE)
-  booking_date = models.DateField()
+  booking_date = models.DateField(null=True)
+  booking_time = models.TimeField(null=True)
   booked_on = models.DateField(auto_now = True)
   
   def __str__(self):
